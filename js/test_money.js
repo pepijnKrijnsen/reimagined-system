29c8977ec993eb57f9745dead6916329f99eb5e1
Pepijn Krijnsen <pepijn@krijnsen.com>
2022-12-25 17:08:23 +0000

    Division works

--- js/test_money.js
+++ js/test_money.js
@@ -9,13 +9,21 @@ class Money {
   times(multiplier) {
     return new Money(this.amount * multiplier, this.currency);
   }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
+  }
 }
 
-let fiver = new Money(5, "USD");
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
 
 let tenEuros = new Money(10, "EUR");
-let twentyEuros = tenEuros.times(2);
-assert.strictEqual(twentyEuros.amount, 20);
-assert.strictEqual(twentyEuros.currency, "EUR");
+let twentyEuros = new Money(20, "EUR");
+assert.deepStrictEqual(tenEuros.times(2), twentyEuros)
+
+let originalMoney = new Money(4002, "KRW")
+let actualMoneyAfterDivision = originalMoney.divide(4)
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
--- py/test_money.py
+++ py/test_money.py
@@ -5,21 +5,33 @@ class Money:
         self.amount = amount
         self.currency = currency
 
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
+
     def times(self, multiplier):
         return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
 
 
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollar(self):
-        fiver = Money(5, "USD")
-        tenner = fiver.times(2)
-        self.assertEqual(tenner.amount, 10)
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        self.assertEqual(fiveDollars.times(2), tenDollars)
 
     def testMultiplicationInEuro(self):
         tenEuros = Money(10, "EUR")
-        twentyEuros = tenEuros.times(2)
-        self.assertEqual(twentyEuros.amount, 20)
-        self.assertEqual(twentyEuros.currency, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(tenEuros.times(2), twentyEuros)
+
+    def testDivision(self):
+        original_money = Money(4002, "KRW")
+        actual_money_after_division = original_money.divide(4)
+        expected_money_after_division = Money(1000.5, "KRW")
+        self.assertEqual(actual_money_after_division,
+                         expected_money_after_division)
 
 
 if __name__ == "__main__":
